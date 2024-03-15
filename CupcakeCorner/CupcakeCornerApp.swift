c1d96c74c7d1b5f87967c90e4b8f8af9604ecbc0
Tony Sharples <tonysharples22@me.com>
2024-03-15 12:59:27 +0000

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Tony Sharples on 15/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
