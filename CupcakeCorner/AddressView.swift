6311d49cf72b7f5c9a2774ff6c6e488e63ecbb9c
Tony Sharples <tonysharples22@me.com>
2024-03-15 14:41:00 +0000

    Create order class and view.

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Tony Sharples on 15/03/2024.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
